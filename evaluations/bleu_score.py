a559e33d28a39eb5b329b123a284fd2f42f67566
Lê Như Đạt <datdev181104@gmail.com>
2026-03-28 15:20:49 +0000

    refine bleu_score

--- evaluations/bleu_score.py
+++ evaluations/bleu_score.py
@@ -1,4 +1,5 @@
 import math as m
+
 from collections import Counter
 
 
@@ -7,19 +8,17 @@ def n_gram(text: str, n: int) -> list:
     words = text.split()
 
     for i in range(len(words) - n + 1):
-        n_gram_list.append(words[i: i + n])
+        n_gram_list.append(tuple(words[i: i + n]))
     
     return n_gram_list
 
 
 def cal_n_gram_match(n_gram_candidate, n_gram_reference) -> int:
-    n_gram_candidate = Counter(tuple(n_gram_candidate))
-    n_gram_reference = Counter(tuple(n_gram_reference))
+    n_gram_candidate_counter = Counter(n_gram_candidate)
+    n_gram_reference_counter = Counter(n_gram_reference)
 
-    print(n_gram_candidate)
-    print(n_gram_reference)
-    pass
-
+    return sum((n_gram_reference_counter & n_gram_candidate_counter).values())
+    
 
 def n_gram_precision_score(candidate: str, reference: str, n: int) -> list:
     n_gram_candidate = n_gram(candidate, n)
@@ -28,36 +27,27 @@ def n_gram_precision_score(candidate: str, reference: str, n: int) -> list:
     n_gram_match = cal_n_gram_match(n_gram_candidate, n_gram_reference)
     total_n_gram_in_prediction = len(n_gram(candidate, n))
 
-    return n_grams_match / total_n_gram_in_prediction
+    if total_n_gram_in_prediction == 0:
+        return 0
+
+    return n_gram_match / total_n_gram_in_prediction
 
 
-def n_gram_precision_list(candidate, reference):
-    pre_list = []
+def geometric_average_precision(candidate, reference):
     
-    pass
-
+    return m.prod([n_gram_precision_score(candidate, reference, i)**(1/4) for i in range(1,5)])
+    
 
 def brevity_penalty_score(length_prediction, length_reference) -> float:
     if length_prediction > length_reference:
         return 1
 
-    return m.exp((1-length_reference) / length_prediction)
-
-
-def geometric_average_precision(n_gram_precision_list: list):
-    res = 1
-    for p_i in n_gram_precision_list:
-        res *= p_i**(1/4) 
-
-    return res
+    return m.exp(1 - (length_reference / length_prediction))
 
 
 def bleu_score(candidate, reference):
-    n_gram_pre_list = n_gram_precision_list(candidate, reference)
-
     BP = brevity_penalty_score(len(candidate), len(reference))
-    geometric_mean = geometric_average_precision(n_gram_pre_list)
+    geometric_mean = geometric_average_precision(candidate, reference)
 
     return BP * geometric_mean
-
 
